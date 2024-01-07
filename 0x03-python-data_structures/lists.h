eb7675ee4f395aaebf4558f45de063bb42652cd7
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-01-07 04:31:43 +0200

    update

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,7 +17,9 @@ int n;
 struct listint_s *next;
 } listint_t;
 
-listint_t *reverse(listint_t *head);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 void print_python_list_info(PyObject *p);
 int is_palindrome(listint_t **head);
 #endif /* LISTS_H */
