2b0fa1831ad3b570d69f7c8e09ee39e704a477ce
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-01-11 03:31:49 +0200

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,9 @@
 #include <time.h>
 #include <stdio.h>
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
 
 void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p);
