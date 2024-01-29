01a13a92e3e125d4786c9e70efb17f9bbca33b10
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-01-30 01:44:41 +0200

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -101,12 +101,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
-
-    def __repr__(self):
-        """Returns a string representation of the `Rectangle`
-           instance that can be used by `eval()` to create a new object.
-
-        Returns:
-            str: The string representation of the `Rectangle` instance.
-        """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
