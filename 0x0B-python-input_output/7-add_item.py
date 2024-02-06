122217588a42a02cb29306334593169de2688f5c
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-02-06 19:10:04 +0200

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 import sys
 from os import path
 
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from save_to_json_file import 5-save_to_json_file
+from load_from_json_file import 6-load_from_json_file
 
 filename = "add_item.json"
 if path.exists(filename):
