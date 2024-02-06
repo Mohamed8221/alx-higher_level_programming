6f5a6a2edb802032d6403a41ec46c8d661178253
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-02-06 19:42:16 +0200

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Module: 7-add_item """
 import sys
-from os import path
+import os
 
 sFile = __import__('5-save_to_json_file.py').save_to_json_file.py
 lFile = __import__('6-load_from_json_file.py').load_from_json_file.py
