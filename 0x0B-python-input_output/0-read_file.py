6c2077b42409baa3c300ed8715af67d501f417a6
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-02-06 17:29:52 +0200

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 0-read_file
+"""
+
+
+def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to "".
+
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
