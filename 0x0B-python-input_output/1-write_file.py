075711db43d0d7683a731bfa548e717b425a5f1a
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-02-06 17:54:02 +0200

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,5 +13,5 @@ def write_file(filename="", text=""):
         int: The number of characters written.
     """
 
-    with open(filename, 'w', encoding'utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
