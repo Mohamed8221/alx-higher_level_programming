a4e7bf59d11a68f9a4d5fbfde1e6bb38a981c0a3
Mohamed8221 <s21511383eg@std.aou.edu.eg>
2024-04-09 16:37:55 +0200

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let narr = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let i = 0; i < 3; i++){
+let narr = ['C is fun', 'Python is cool', 'JavaScript is amazing']; 
+for (i = 0; i < 3; i++){
     console.log (narr[i])
 }
